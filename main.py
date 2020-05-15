ab1e1dfbada24c094748b152c37020d6d3359242
jotavaladouro <jotavaladouro@gmail.com>
2020-05-15 09:18:41 +0200

    Add special dates

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ SPECIAL_DATES = [{"date": dt.datetime(2020, 3, 14), "label": 'Soft Confinement',
                  {"date": dt.datetime(2020, 3, 30), "label": 'Hard Confinement', "color": "r"},
                  {"date": dt.datetime(2020, 4, 13), "label": 'Soft Confinement', "color": "orange"},
                  {"date": dt.datetime(2020, 4, 26), "label": 'Child walk', "color": "blue"},
-                 {"date": dt.datetime(2020, 5, 2),  "label": 'Walk and sport', "color": "green"}]
+                 {"date": dt.datetime(2020, 5, 2),  "label": 'Walk and sport', "color": "green"},
+                 {"date": dt.datetime(2020, 5, 10),  "label": 'Phase 1', "color": "green"}]
 CA_REMOVE = ["ML", "CE"]
 DATA_REMOVE_IN_DIFF = ["2020-04-26","2020-4-11"]
 CA_SPECIAL_CODE = [{"NAME": "Madrid", "CODE": "MD"},
